77a27e810c9a3fa83981b9ff3cc4387cd6f6efc0
양지원 <jina_8151010@naver.com>
2024-09-23 18:32:29 +0900

    feature: health check controller

---
+++ backend/src/main/java/LuckyVicky/backend/global/controller/healthController.java
@@ -0,0 +1,14 @@
+package LuckyVicky.backend.global.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+
+public class healthController {
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
