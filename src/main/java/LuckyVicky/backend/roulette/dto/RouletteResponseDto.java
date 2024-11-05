1f4a99c5d35bcf1ed87f78b4197f5f73e4949604
ldh6434 <ldh6434@naver.com>
2024-11-06 01:21:06 +0900

    룰렛 스핀 응답 형식을 위한 RouletteResponseDto 생성

--- src/main/java/LuckyVicky/backend/roulette/dto/RouletteResponseDto.java
+++ src/main/java/LuckyVicky/backend/roulette/dto/RouletteResponseDto.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-public class RouletteDto {
+public class RouletteResponseDto {
 
     @Schema(description = "룰렛 결과 DTO")
     @Getter
@@ -19,5 +19,18 @@ public class RouletteDto {
 
         @Schema(description = "받은 보석 개수", example = "1")
         private int jewelCount;
+    }
+
+    @Schema(description = "룰렛 가능 여부 DTO")
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class RouletteAvailableDto {
+        @Schema(description = "룰렛을 돌릴 수 있는지 여부", example = "true")
+        private boolean available;
+
+        @Schema(description = "남은 대기 시간 (초 단위)", example = "600")
+        private int remainingTime;
     }
 }
