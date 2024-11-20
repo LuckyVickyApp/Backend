e03792ce6065e225336e481065b40f891c8fc6fe
persi <jina8151010@gmai.com>
2024-11-20 19:33:50 +0900

    chore: conflict 해결

--- src/main/java/LuckyVicky/backend/global/util/Constant.java
+++ src/main/java/LuckyVicky/backend/global/util/Constant.java
@@ -2,6 +2,7 @@ package LuckyVicky.backend.global.util;
 
 public class Constant {
     public static final String PHONE_NUMBER_PATTERN = "^\\d{11}$";
+    public static final String AES_PHONE_NUMBER_TRANSFORMATION = "AES/CBC/PKCS5Padding";
     public static final String CONVERTER_INSTANTIATION_NOT_ALLOWED = "Converter class는 인스턴스화가 불가능합니다.";
 
 }
