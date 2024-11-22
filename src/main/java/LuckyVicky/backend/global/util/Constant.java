b52fd3d5c71c348430c8aab77c149f2a4a2fa5f1
persi <jina8151010@gmai.com>
2024-11-22 17:31:50 +0900

    chore: 출석부 관련 상수 추가

--- src/main/java/LuckyVicky/backend/global/util/Constant.java
+++ src/main/java/LuckyVicky/backend/global/util/Constant.java
@@ -1,8 +1,15 @@
 package LuckyVicky.backend.global.util;
 
 public class Constant {
+
+    // Phone
     public static final String PHONE_NUMBER_PATTERN = "^\\d{11}$";
     public static final String AES_PHONE_NUMBER_TRANSFORMATION = "AES/CBC/PKCS5Padding";
+
+    // Attendance
+    public static final int ATTENDANCE_CYCLE_DAYS = 12;
+
+    // Notice
     public static final String CONVERTER_INSTANTIATION_NOT_ALLOWED = "Converter class는 인스턴스화가 불가능합니다.";
 
 }
