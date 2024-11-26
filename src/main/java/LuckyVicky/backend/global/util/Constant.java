b54637072cc558cc7f7fad1edca9f5eb6936666e
persi <jina8151010@gmai.com>
2024-11-27 01:07:26 +0900

    chore: Fcm 위한 설정

--- src/main/java/LuckyVicky/backend/global/util/Constant.java
+++ src/main/java/LuckyVicky/backend/global/util/Constant.java
@@ -1,8 +1,13 @@
 package LuckyVicky.backend.global.util;
 
+import com.google.api.client.util.Value;
 import java.time.format.DateTimeFormatter;
 
 public class Constant {
+
+    @Value("${fcm.project-id}")
+    private static String FCM_PROJECT_ID;
+
     // Log
     public static final String LOG_S3_DIRECTORY = "logs/";
     public static final String LOG_LOCAL_FILE_DIRECTORY = "src/main/resources/logs/";
@@ -21,6 +26,10 @@ public class Constant {
 
     // Fcm
     public static final int DEVICE_REGISTRATION_LIMIT = 3;
+    public static final String FCM_API_URL =
+            "https://fcm.googleapis.com/v1/projects/" + FCM_PROJECT_ID + "/messages:send";
+    public static final String FIREBASE_SECRET_KEY_PATH = "firebase/serviceAccountKey.json";
+
 
     // Notice
     public static final String CONVERTER_INSTANTIATION_NOT_ALLOWED = "Converter class는 인스턴스화가 불가능합니다.";
