e0e98f52dd567f14a221948d879cc8504ab1efc7
persi <jina8151010@gmai.com>
2024-12-16 23:55:20 +0900

    chore: uuid 생성 로직 util로 빼기

---
+++ src/main/java/LuckyVicky/backend/global/util/UuidGenerator.java
@@ -0,0 +1,15 @@
+package LuckyVicky.backend.global.util;
+
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+import java.util.UUID;
+
+public class UuidGenerator {
+
+    public static String generateNanoUuid() {
+        return NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR, NanoIdUtils.DEFAULT_ALPHABET, 12);
+    }
+
+    public static String generateUuid() {
+        return UUID.randomUUID().toString();
+    }
+}
