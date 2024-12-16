daff838cd99ad4df93e83de60e9a970da2ff333d
persi <jina8151010@gmai.com>
2024-12-16 23:57:02 +0900

    refactor: uuid를 따로 저장할 필요가 없어 repository와 entity 제거

--- src/main/java/LuckyVicky/backend/global/repository/UuidRepository.java
+++
@@ -1,8 +0,0 @@
-package LuckyVicky.backend.global.repository;
-
-import LuckyVicky.backend.global.entity.Uuid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UuidRepository extends JpaRepository<Uuid, Long> {
-
-}
