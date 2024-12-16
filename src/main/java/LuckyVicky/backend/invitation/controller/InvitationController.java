febad71d5df9591313eee65c20c71ae8df20770a
persi <jina8151010@gmai.com>
2024-12-17 02:46:09 +0900

    feat: 추천코드가 타인에 의해 작성되었을 경우, 초대자에게 알림 전송

--- src/main/java/LuckyVicky/backend/invitation/controller/InvitationController.java
+++ src/main/java/LuckyVicky/backend/invitation/controller/InvitationController.java
@@ -1,21 +1,23 @@
 package LuckyVicky.backend.invitation.controller;
 
+import LuckyVicky.backend.global.api_payload.ApiResponse;
+import LuckyVicky.backend.global.api_payload.SuccessCode;
+import LuckyVicky.backend.invitation.dto.InvitationRequestDto;
+import LuckyVicky.backend.invitation.sevice.InvitationService;
 import LuckyVicky.backend.user.domain.User;
 import LuckyVicky.backend.user.jwt.CustomUserDetails;
 import LuckyVicky.backend.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import LuckyVicky.backend.global.api_payload.ApiResponse;
-import LuckyVicky.backend.global.api_payload.SuccessCode;
-import LuckyVicky.backend.invitation.sevice.InvitationService;
-import LuckyVicky.backend.invitation.dto.InvitationRequestDto;
+
 @Tag(name = "초대", description = "초대 관련 API입니다.")
 @RestController
 @RequiredArgsConstructor
@@ -33,7 +35,7 @@ public class InvitationController {
     public ApiResponse<String> acceptInvitation(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody InvitationRequestDto.InvitationReqDto invitationReqDto
-    ){
+    ) throws IOException {
         User user = userService.findByUserName(customUserDetails.getUsername());
         String owner = invitationService.acceptInvitation(user, invitationReqDto.getInvitationCode());
 
