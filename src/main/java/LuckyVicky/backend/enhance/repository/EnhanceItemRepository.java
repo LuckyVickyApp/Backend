97a5473be9ff09700f9d82a0826a70498ff476d0
persi <jina8151010@gmai.com>
2025-01-16 03:44:37 +0900

    refactor: EntityGraph를 사용해 필요한 경우에만 연관 데이터를 로드

--- src/main/java/LuckyVicky/backend/enhance/repository/EnhanceItemRepository.java
+++ src/main/java/LuckyVicky/backend/enhance/repository/EnhanceItemRepository.java
@@ -5,6 +5,7 @@ import LuckyVicky.backend.item.domain.Item;
 import LuckyVicky.backend.user.domain.User;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +14,14 @@ public interface EnhanceItemRepository extends JpaRepository<EnhanceItem, Long> 
 
     Optional<EnhanceItem> findByUserAndItem(User user, Item item);
 
+    @EntityGraph(attributePaths = {"user"}) // N+1 문제 해결 위한 EntityGraph 방식
+    @Query("SELECT e FROM EnhanceItem e WHERE e.item = :item ORDER BY e.enhanceLevel DESC, e.enhanceLevelReachedAt ASC")
+    List<EnhanceItem> findEnhanceItemsByItemOrderByEnhanceLevelAndReachedTime(@Param("item") Item item);
+
+    /* N+1 문제 해결 위한 JOIN FETCH 방식
     @Query("SELECT e FROM EnhanceItem e JOIN FETCH e.user WHERE e.item = :item ORDER BY e.enhanceLevel DESC, e.enhanceLevelReachedAt ASC")
     List<EnhanceItem> findEnhanceItemsByItemOrderByEnhanceLevelAndReachedTime(@Param("item") Item item);
+     */
 
     Integer countByItem(Item item);
 }
