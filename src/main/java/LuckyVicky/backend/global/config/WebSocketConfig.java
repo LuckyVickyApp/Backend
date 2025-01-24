67e90d12c3a08dcbd28cbd3eec915ed4271f7066
persi <jina8151010@gmai.com>
2025-01-24 17:34:22 +0900

    refactor: 여러 트랜잭션이 동시에 엔티티를 생성하려고 할 경우 하나만 성공하고 나머지는 예외 발생하는데, 에러 발생 시 재시도 로직 실행

--- src/main/java/LuckyVicky/backend/global/config/WebSocketConfig.java
+++ src/main/java/LuckyVicky/backend/global/config/WebSocketConfig.java
@@ -4,12 +4,14 @@ import LuckyVicky.backend.display_board.handler.DisplayBoardWebSocketHandler;
 import LuckyVicky.backend.pachinko.handler.PachinkoWebSocketHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
+@EnableRetry
 @AllArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
     private final PachinkoWebSocketHandler pachinkoWebSocketHandler;
